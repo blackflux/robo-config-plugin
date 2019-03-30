9ce8283d6540bc081297f124e5861bd1e7d4151c
Lukas Siemon <lukas.siemon@gmail.com>
2019-03-30 16:00:38 -0700

    fix: updated deps

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const expect = require('chai').expect;
+const { load } = require('robo-config');
 const plugin = require('../src/index');
 
 it('Documenting Plugin Tasks', () => {
-  expect(plugin.syncDocs()).to.deep.equal([]);
+  expect(load(plugin).syncDocs()).to.deep.equal([]);
 });
 
 it('Testing Plugin Tasks', () => {
-  expect(plugin.test(path.join(__dirname, 'project'), {
+  expect(load(plugin).test(path.join(__dirname, 'project'), {
     repoKey: 'blackflux/robo-config',
     packageName: 'robo-config',
     repoName: 'robo-config',
