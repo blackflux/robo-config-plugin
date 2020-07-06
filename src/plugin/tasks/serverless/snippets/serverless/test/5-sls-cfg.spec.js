9a464acec3e426406f12be2715ba22febe2e6ba4
Lukas Siemon <github@blackflux.com>
2020-07-06 12:57:58 -0700

    feat: now generating sls cfg test

---
+++ src/plugin/tasks/serverless/snippets/serverless/test/5-sls-cfg.spec.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const https = require('https');
+const fs = require('smart-fs');
+const expect = require('chai').expect;
+const { describe } = require('node-tdd');
+
+describe('Testing sls configuration', { timeout: 10000 }, () => {
+  it('Testing aws-sdk version', async () => {
+    const r = await new Promise((resolve) => https.get(
+      'https://docs.aws.amazon.com/lambda/latest/dg/lambda-runtimes.html',
+      (resp) => {
+        let data = '';
+        resp.on('data', (chunk) => {
+          data += chunk;
+        });
+        resp.on('end', () => {
+          resolve(data);
+        });
+      }
+    ));
+    const version = r.match(
+      /<td>\s+<p><code class="code">nodejs12\.x<\/code><\/p>\s+<\/td>\s+<td>\s+<p>(\d+\.\d+\.\d+)<\/p>\s+<\/td>/
+    )[1];
+    const pkg = fs.smartRead(path.join(__dirname, '..', 'package.json'));
+    expect(pkg.devDependencies['aws-sdk'], `devDependencies.aws-sdk !== $\{version}`).to.equal(version);
+    expect(pkg.peerDependencies['aws-sdk'], `peerDependencies.aws-sdk !== $\{version}`).to.equal(version);
+  });
+});
