86f3035637393e5e64e5105eb2419a1b67a936f1
Ezequiel Alanis <ealanisg@gmail.com>
2020-07-21 15:32:44 -0700

    amend: split setting

--- src/plugin/tasks/serverless/snippets/serverless/test/0-sls-cf-stack.spec.js
+++ src/plugin/tasks/serverless/snippets/serverless/test/0-sls-cf-stack.spec.js
@@ -61,8 +61,11 @@ describe('Testing serverless cf stack definitions', { cryptoSeed: 'seed' }, () =
     objectScan(['resources.Resources.*.Type'], {
       filterFn: ({ value, parent }) => {
         if (value === 'AWS::DynamoDB::Table') {
-          const setting = get(parent,
-            'Properties.PointInTimeRecoverySpecification.PointInTimeRecoveryEnabled');
+          const setting = get(parent, [
+            'Properties',
+            'PointInTimeRecoverySpecification',
+            'PointInTimeRecoveryEnabled'
+          ]);
           expect([true, false].includes(setting),
             'DynamoDB Point-in-time recovery should be explicitly specified.')
             .to.deep.equal(true);
