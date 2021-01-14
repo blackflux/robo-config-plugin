7ba92762e53383d232b2cb7308d17938fe177b54
Lukas Siemon <github@blackflux.com>
2021-01-13 16:48:46 -0800

    fix: coverage issue

--- test/projects/assorted/@sls-closedsource-dynamodb/test/9-sls-dynamodb-local.spec.js
+++ test/projects/assorted/@sls-closedsource-dynamodb/test/9-sls-dynamodb-local.spec.js
@@ -5,9 +5,27 @@ const { describe } = require('node-tdd');
 const fs = require('smart-fs');
 const expect = require('chai').expect;
 
+const normalize = (table) => objectScan([
+  '*',
+  '!PointInTimeRecoverySpecification',
+  '!TimeToLiveSpecification'
+], {
+  reverse: false,
+  filterFn: ({ property, value, context }) => {
+    if (property === 'TableName') {
+      // eslint-disable-next-line no-template-curly-in-string
+      context[property] = value.replace('${self:provider.stage}', 'local');
+    } else if (property === 'StreamSpecification') {
+      context[property] = { StreamEnabled: true, ...value };
+    } else {
+      context[property] = value;
+    }
+  }
+})(table, {});
+
 describe('Testing dynamodb-local', { envVarsFile: 'env.yml' }, () => {
   let dataStack;
-  let tables = [];
+  let tables;
   before(() => {
     dataStack = yaml.load(
       path.join(__dirname, '..', 'serverless.wrapper.yml'),
@@ -26,34 +44,26 @@ describe('Testing dynamodb-local', { envVarsFile: 'env.yml' }, () => {
     })(dataStack, []);
   });
 
+  it('Testing normalize table', () => {
+    expect(normalize({ StreamSpecification: {} }))
+      .to.deep.equal({ StreamSpecification: { StreamEnabled: true } });
+  });
+
   it('Testing tables set-up', async () => {
     const result = ['#!/bin/sh'];
-    tables.forEach((table) => {
-      const t = Object.fromEntries(Object.entries(table)
-        .filter(([k]) => [
-          'PointInTimeRecoverySpecification',
-          'TimeToLiveSpecification'
-        ].includes(k) === false)
-        .map(([k, v]) => {
-          if (k === 'TableName') {
-            // eslint-disable-next-line no-template-curly-in-string
-            return [k, v.replace('${self:provider.stage}', 'local')];
-          }
-          if (k === 'StreamSpecification') {
-            return [k, { StreamEnabled: true, ...v }];
-          }
-          return [k, v];
-        }));
-      result.push(
-        `AWS_ACCESS_KEY_ID=${process.env.AWS_ACCESS_KEY_ID} \\`,
-        `AWS_SECRET_ACCESS_KEY=${process.env.AWS_SECRET_ACCESS_KEY} \\`,
-        `AWS_REGION=${process.env.AWS_REGION} \\`,
-        'aws dynamodb create-table \\',
-        ` --cli-input-json '${JSON.stringify(t)}' \\`,
-        ' --endpoint-url http://dynamodb-local:8000 \\',
-        ` --region ${process.env.AWS_REGION}`
-      );
-    });
+    tables
+      .map((table) => normalize(table))
+      .forEach((table) => {
+        result.push(
+          `AWS_ACCESS_KEY_ID=${process.env.AWS_ACCESS_KEY_ID} \\`,
+          `AWS_SECRET_ACCESS_KEY=${process.env.AWS_SECRET_ACCESS_KEY} \\`,
+          `AWS_REGION=${process.env.AWS_REGION} \\`,
+          'aws dynamodb create-table \\',
+          ` --cli-input-json '${JSON.stringify(table)}' \\`,
+          ' --endpoint-url http://dynamodb-local:8000 \\',
+          ` --region ${process.env.AWS_REGION}`
+        );
+      });
     expect(
       fs.smartWrite(path.join(__dirname, '..', 'docker', 'dynamodb.sh'), result),
       'DynamoDb tables updated. Please re-run.'
