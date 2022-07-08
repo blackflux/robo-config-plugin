082c183de96669fb2570ad53a00d3f7fac13a43a
Lukas Siemon <lukas.siemon@blackflux.com>
2022-07-08 11:38:33 -0700

    fix: adjusted queue.spec.js

--- test/projects/assorted/@sls-queue/test/queue/queue.spec.js
+++ test/projects/assorted/@sls-queue/test/queue/queue.spec.js
@@ -1,12 +1,13 @@
-const path = require('path');
-const { expect } = require('chai');
-const { describe } = require('node-tdd');
-const fs = require('smart-fs');
-const processor = require('../../src/queue/queue');
+import path from 'path';
+import { expect } from 'chai';
+import { describe } from 'node-tdd';
+import fs from 'smart-fs';
 
-describe('Testing queue', () => {
+describe('Testing queue', { envVarsFile: '../env-vars.yml' }, () => {
   it('Testing digraph generation', async () => {
-    const digraphFile = path.join(__dirname, '..', '..', 'src', 'queue', 'digraph.dot');
+    process.env.TEST_SEED = Math.random();
+    const { default: processor } = await import('../../src/queue/queue.js');
+    const digraphFile = path.join(fs.dirname(import.meta.url), '..', '..', 'src', 'queue', 'digraph.dot');
     const digraph = processor.digraph();
     const result = fs.smartWrite(digraphFile, digraph, { treatAs: 'txt' });
     expect(result, 'Digraph file updated').to.equal(false);
