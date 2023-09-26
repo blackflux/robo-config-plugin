24ff2f6fe18972b9212a8abdb7d2070d702a74b6
Lukas Siemon <lukas.siemon@blackflux.com>
2023-09-26 15:54:02 -0700

    feat: now allowing custom command to docker container

--- src/plugin/tasks/docker/snippets/sls-react/manage.sh
+++ src/plugin/tasks/docker/snippets/sls-react/manage.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
+command=bash
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -c|--command) command="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; shift ;;
+    esac
+    shift
+done
+
 docker build \
+  --build-arg COMMAND="$command" \
   -t lambda-environment-node \
   --network="host" \
   docker/. &&
--- test/projects/assorted/@dynamo-testing/manage.sh
+++ test/projects/assorted/@dynamo-testing/manage.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
+command=bash
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -c|--command) command="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; shift ;;
+    esac
+    shift
+done
+
 docker run \
   --name dynamodb-local \
   -p 0.0.0.0:8000:8000 \
   -d amazon/dynamodb-local
 
 docker build \
+  --build-arg COMMAND="$command" \
   -t lambda-environment-node \
   --network="host" \
   docker/. &&
